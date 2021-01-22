03d58e2eacc444ea85c50f82325b0d4b041ca2d5
Yaroslav <yaroslav.kirakosian@gmail.com>
2021-01-22 18:14:48 +0200

    Implemented component WeeksChallengeTasks

---
+++ src/components/weeks-challenge/weeks-challenge-task/WeeksChallengeTask.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WeeksChallengeTask.scss";
+
+const WeeksChallengeTasks = () => {
+  return (
+    <ol className="weeks-challenge__check-list">
+      <li className="weeks-challenge__check-item">
+        <p className="weeks-challenge__check-desc">Utför veckans uppgifter</p>
+        <img className="weeks-challenge__check-icon" src="#" alt="done" />
+      </li>
+      <li className="weeks-challenge__check-item">
+        <p className="weeks-challenge__check-desc">Utför veckans uppgifter</p>
+        <img className="weeks-challenge__check-icon" src="#" alt="done" />
+      </li>
+    </ol>
+  );
+};
+
+export default WeeksChallengeTasks;
