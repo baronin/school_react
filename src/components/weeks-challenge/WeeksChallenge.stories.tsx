8d36ee0deed19c9d98fcd6242e1fe7936db580ac
Yaroslav <yaroslav.kirakosian@gmail.com>
2021-02-02 21:08:24 +0200

    renamed files

--- src/components/weeks-challenge/WeeksChallenge.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import WeeksChallenge from './WeeksChallenge';
-import data from '../../mocked-data/mission-user-path-data'
-
-storiesOf('WeeksChallenge', module)
-	.add('WeeksChallenge', () => <WeeksChallenge {...data} />)
