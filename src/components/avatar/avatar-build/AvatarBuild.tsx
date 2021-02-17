9cf8a11b44a331496b438853196cc0b0d2a2b5ad
Yaroslav <yaroslav.kirakosian@beetroot.com.ua>
2021-02-17 18:03:34 +0200

    Added images to AvatarBuild

--- src/components/avatar/avatar-build/AvatarBuild.tsx
+++ src/components/avatar/avatar-build/AvatarBuild.tsx
@@ -1,16 +1,51 @@
 import { FC } from 'react';
 import { AvatarData } from '../../../types/types';
 
+import './AvatarBuild.scss';
+
 const AvatarBuild: FC<AvatarData> = (props) => {
-	const { body, eyes, hat, hair } = props;
+	const { body, hair, eyes, nose, mouth, hat, glasses, shirt, special } = props;
+
 	return (
-		<div className="avatar__body">
-			<h3>Body</h3>
-			<div className="avatar__build">
-				{body && <img src={body?.iconUrl} alt="" />}
-				{hair && <img src={hair?.iconUrl} alt="" />}
-				{hat && <img src={hat?.iconUrl} alt="" />}
-				{eyes && <img src={eyes?.iconUrl} alt="" />}
+		<div className="avatar__build">
+			<h3 className="avatar__build-title">Body</h3>
+			<div className="avatar__build-wrap">
+				{body &&
+					body?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{hair &&
+					hair?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{eyes &&
+					eyes?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{nose &&
+					nose?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{mouth &&
+					mouth?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{hat &&
+					hat?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{glasses &&
+					glasses?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{shirt &&
+					shirt?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
+				{special &&
+					special?.components.map((item, idx) => (
+						<img src={item.url} key={idx} alt="test" style={{ zIndex: item.layer }} />
+					))}
 			</div>
 		</div>
 	);
