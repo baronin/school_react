794be5c1c028eac3089e0b28c6e6cc562d90f1d3
EvgeniiShevchenko <evgenii.shevchenko@outlook.com>
2021-02-17 18:03:13 +0200

    fix minor bugs

--- src/components/mission/remaining-time/RemainingTime.stories.tsx
+++ src/components/mission/remaining-time/RemainingTime.stories.tsx
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import RemainingTime from './RemainingTime';
 import './../../assets/scss/reset.scss';
 storiesOf('RemainingTime', module)
-	.add('5', () => <RemainingTime date="5" />)
-	.add('4', () => <RemainingTime date="4" />);
+	.add('5', () => <RemainingTime incomingDate={new Date()} />)
+	.add('4', () => <RemainingTime incomingDate={new Date()} />);
--- src/components/mission/remaining-time/RemainingTime.tsx
+++ src/components/mission/remaining-time/RemainingTime.tsx
@@ -1,18 +1,45 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './RemainingTime.scss';
 import timerIcon from '../../../assets/images/general-icons/timer.png';
 
 interface Props {
-	date: number | string | undefined;
+	incomingDate: Date | undefined;
 }
 
-const RemainingTime: FC<Props> = ({ date }) => {
-	console.log('RemaingTime', date);
+const RemainingTime: FC<Props> = ({ incomingDate }) => {
+	// work with data
+	const [timer, setTimer] = useState(0);
+
+	const amountMmInDay = 86400000;
+	const today = new Date().toDateString();
+	const todayParseMs = Date.parse(today);
+	const parseResetDate = Date.parse(incomingDate?.toDateString() as string);
+	const getRemainingDays = (parseResetDate - todayParseMs) / (60 * 60 * 24 * 1000);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			console.log('This will run every second!');
+			setTimer((seconds) => seconds + 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	const getTransformTimeToFormatHhMm = () => {
+		console.log(111111111111);
+		const restTime = parseResetDate - todayParseMs;
+
+		return restTime - timer;
+	};
+
+	console.log('RemaingTime', getRemainingDays);
 	// count down
 	return (
 		<div className="time-left">
 			<img className="time-left__icon" src={timerIcon} alt="timer" width="15px" height="15px" />
-			<span className="time-left__text">{date}d kvar</span>
+			<span className="time-left__text">
+				{parseResetDate - todayParseMs > amountMmInDay ? timer : getTransformTimeToFormatHhMm()}d kvar
+			</span>
 		</div>
 	);
 };
