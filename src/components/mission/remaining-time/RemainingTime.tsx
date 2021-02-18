eb7c4ad63389740c9b0d24799a69818eb2eb7d20
Evgenii <evgenii.shevchenko@outlook.com>
2021-02-18 10:35:24 +0200

    fix minor bugs

--- src/components/mission/remaining-time/RemainingTime.tsx
+++ src/components/mission/remaining-time/RemainingTime.tsx
@@ -11,14 +11,16 @@ const RemainingTime: FC<Props> = ({ incomingDate }) => {
 	const [timer, setTimer] = useState(0);
 
 	const amountMmInDay = 86400000;
-	const today = new Date().toDateString();
-	const todayParseMs = Date.parse(today);
-	const parseResetDate = Date.parse(incomingDate?.toDateString() as string);
+	// const today = new Date().toDateString();
+	// console.log('🚀 ~ file: RemainingTime.tsx ~ line 15 ~ today', today);
+	const todayParseMs = Date.now();
+	console.log('🚀 ~ file: RemainingTime.tsx ~ line 16 ~ todayParseMs', todayParseMs / 1000);
+	const parseResetDate = 1613599199 * 1000;
 	const getRemainingDays = (parseResetDate - todayParseMs) / (60 * 60 * 24 * 1000);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			console.log('This will run every second!');
+			console.log('This will run every second!', incomingDate);
 			setTimer((seconds) => seconds + 1);
 		}, 1000);
 
@@ -27,9 +29,13 @@ const RemainingTime: FC<Props> = ({ incomingDate }) => {
 
 	const getTransformTimeToFormatHhMm = () => {
 		console.log(111111111111);
-		const restTime = parseResetDate - todayParseMs;
+		const restTime = (parseResetDate - todayParseMs) / 1000;
+		console.log('🚀 ~ file: RemainingTime.tsx ~ line 31 ~ getTransformTimeToFormatHhMm ~ restTime', restTime, timer);
+		const hours = Math.floor(restTime / 3600);
+		const minutes = Math.ceil((restTime % 3600) / 60);
 
-		return restTime - timer;
+		console.log('🚀 ~ file: RemainingTime.tsx ~ line 35 ~ getTransformTimeToFormatHhMm ~ minutes', hours, minutes);
+		return `${hours}:${minutes}`;
 	};
 
 	console.log('RemaingTime', getRemainingDays);
@@ -38,7 +44,10 @@ const RemainingTime: FC<Props> = ({ incomingDate }) => {
 		<div className="time-left">
 			<img className="time-left__icon" src={timerIcon} alt="timer" width="15px" height="15px" />
 			<span className="time-left__text">
-				{parseResetDate - todayParseMs > amountMmInDay ? timer : getTransformTimeToFormatHhMm()}d kvar
+				{parseResetDate - todayParseMs > amountMmInDay
+					? getTransformTimeToFormatHhMm()
+					: getTransformTimeToFormatHhMm()}
+				d kvar
 			</span>
 		</div>
 	);
