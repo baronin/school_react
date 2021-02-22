a6b508dbc8bbf9ed24194f806341ffa3ae53caa1
Yaroslav <yaroslav.kirakosian@gmail.com>
2021-02-22 10:50:32 +0200

    updated styles for avatar buttons

--- src/components/AvatarBuilder/AvatarModal/AvatarModal.tsx
+++ src/components/AvatarBuilder/AvatarModal/AvatarModal.tsx
@@ -8,8 +8,12 @@ const AvatarModal: FC = () => {
 			<div className="avatar-modal__inner">
 				<h1 className="avatar-modal__title">Save changes?</h1>
 				<div>
-					<button type="button">No</button>
-					<button type="submit">Yes</button>
+					<button className="avatar-modal__button" type="button">
+						No
+					</button>
+					<button className="avatar-modal__button" type="submit">
+						Yes
+					</button>
 				</div>
 			</div>
 		</div>
